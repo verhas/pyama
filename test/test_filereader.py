9bfdd9fc3671a79c6b7892fa46dc13c060ff2574
Peter Verhas <peter@verhas.com>
2018-02-25 22:44:05 +0100

    some unit test fixup

--- test/test_filereader.py
+++ test/test_filereader.py
@@ -15,9 +15,22 @@ class TestFileReader(unittest.TestCase):
                      ("PYTHON\s+SEGMENT\s+(\\w[\\w\\d_]*)",None)]
             )
         file = reader.read()
-        for segment in file.segments:
-            print("... segment '%s'" % segment.name)
-            print(">>>%s<<<" % "".join(segment.text))
+        self.assertEqual(7,len(file.segments))
+        self.assertEqual('0',file.segments[0].name)
+        self.assertEqual(2,len(file.segments[0].text))
+        self.assertEqual('segmentOne',file.segments[1].name)
+        self.assertEqual(3,len(file.segments[1].text))
+        self.assertEqual('1',file.segments[2].name)
+        self.assertEqual(1,len(file.segments[2].text))
+        self.assertEqual('anotherSegment',file.segments[3].name)
+        self.assertEqual(6,len(file.segments[3].text))
+        self.assertEqual('2',file.segments[4].name)
+        self.assertEqual(2,len(file.segments[4].text))
+        self.assertEqual('python_segment',file.segments[5].name)
+        self.assertEqual(4,len(file.segments[5].text))
+        self.assertEqual('python_segment',file.segments[6].name)
+        self.assertEqual(3,len(file.segments[6].text))
+
 
 if __name__ == '__main__':
     unittest.main()
