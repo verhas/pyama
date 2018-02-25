ca36a63bd140b1f120add69776f79ed4da2f4726
Peter Verhas <peter@verhas.com>
2018-02-25 22:36:35 +0100

    some unit test fixup

---
+++ test/test_collector.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python
+# -*- coding: utf8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+import unittest
+
+from pyama.collector import FileCollector
+from pyama.configuration import Configuration
+
+
+class TestFileCollector(unittest.TestCase):
+    def testCollectsFiles(self):
+        files = FileCollector([Configuration().file("\\.snip$")], "**/*.*").collect()
+        self.assertEqual(1, len(files), "There is only one *.snip file")
+        self.assertEqual("snippet_test.snip", [x for x in files][0], "The file is snippet_test.snip")
+
+
+if __name__ == '__main__':
+    unittest.main()
