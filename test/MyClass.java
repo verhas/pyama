c29860b9be4f56212bb643da84369a61a928f0e6
Peter Verhas <peter@verhas.com>
2018-02-28 17:27:06 +0100

    JavaDoc added to test stopper condition

--- test/MyClass.java
+++ test/MyClass.java
@@ -13,8 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import java.util.Map;
 
+/**
+ * This is a Javadoc
+ *
+ */
 public class MyClass implements ScriptBasic {
 
     // FIELDS
