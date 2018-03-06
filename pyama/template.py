aead08bd7e55714a839c129926053bfc4c4ca62b
Peter Verhas <peter@verhas.com>
2018-03-06 16:07:40 +0100

    repeat can have separation argument between the `repeat` and the `:` bug fixed

--- pyama/template.py
+++ pyama/template.py
@@ -1,4 +1,5 @@
 import string
+import re
 
 
 class SnippetFormatter(string.Formatter):
@@ -7,17 +8,25 @@ class SnippetFormatter(string.Formatter):
        https://makina-corpus.com/blog/metier/2016/the-worlds-simplest-python-template-engine
        """
 
-    def format_field(self, value, spec):
-        if spec.startswith('repeat:'):
-            template = spec.partition(':')[-1]
-            if type(value) is dict:
-                value = value.items()
-            return ''.join([template.format(item=item) for item in value])
+    def __init__(self):
+        super(SnippetFormatter, self).__init__()
+
+    def format_field(self, value_string, spec):
+        match = re.search("repeat([^:]*):(.*)", spec, re.DOTALL) or \
+                re.search("repeat'(.*?)':(.*)", spec, re.DOTALL) or \
+                re.search('repeat"(.*?)":(.*)', spec, re.DOTALL)
+        if match:
+            template = match.group(2)
+            if type(value_string) is dict:
+                text = [self.format(template,key=key, value=value) for key, value in value_string.items()]
+                result = match.group(1).join(text)
+                return result
+            return match.group(1).join([template.format(item=item) for item in value_string])
         elif spec == 'call':
-            return value()
+            return value_string()
         elif spec.startswith('if:'):
-            return (value and spec.partition(':')[-1]) or ''
+            return (value_string and spec.partition(':')[-1]) or ''
         elif spec.startswith('ifnot:'):
-            return ((not value) and spec.partition(':')[-1]) or ''
+            return ((not value_string) and spec.partition(':')[-1]) or ''
         else:
-            return super(SnippetFormatter, self).format_field(value, spec)
+            return super(SnippetFormatter, self).format_field(value_string, spec)
