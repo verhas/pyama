800c4c43eb659bf067b1f322b3b8878610c1a81a
Peter Verhas <peter@verhas.com>
2018-05-08 15:17:06 +0200

    TRIM functionality refactored into separate methods

--- pyama/regexhandler.py
+++ pyama/regexhandler.py
@@ -76,28 +76,39 @@ class RegexHandler(SegmentHandler):
 
         trim_size = 0
         if trim:
-            i = trim_start
-            while i < len(segment.text) - 1:
-                if len(segment.text[i]) > trim_size:
-                    trim_size = len(segment.text[i])
-                i += 1
-            i = trim_start
-            while i < len(segment.text) - 1:
-                space_nr = len(segment.text[i]) - len(segment.text[i].lstrip())
-                if len(segment.text[i].lstrip()) > 0 and trim_size > space_nr:
-                    trim_size = space_nr
-                i += 1
-            i = trim_start
-            while i < len(segment.text) - 1:
-                if trim_size > 0 and len(segment.text[i].lstrip()) > 0 :
-                    segment.text[i] = segment.text[i][trim_size:]
-                i += 1
+            trim_size = self.max_line_length(segment, trim_size, trim_start)
+            trim_size = self.calculate_trim_size(segment, trim_size, trim_start)
+            self.trim_lines(segment, trim_size, trim_start)
 
         # save the user from shooting the foot
         # may accidentally remove the new-line characters, but even then the last line has to have a new line
         if len(segment.text[-2]) == 0 or segment.text[-2][-1] != "\n":
             segment.text[-2] = segment.text[-2] + "\n"
         return segment.text
+
+    def trim_lines(self, segment, trim_size, trim_start):
+        i = trim_start
+        while i < len(segment.text) - 1:
+            if trim_size > 0 and len(segment.text[i].lstrip()) > 0:
+                segment.text[i] = segment.text[i][trim_size:]
+            i += 1
+
+    def calculate_trim_size(self, segment, trim_size, trim_start):
+        i = trim_start
+        while i < len(segment.text) - 1:
+            space_nr = len(segment.text[i]) - len(segment.text[i].lstrip())
+            if len(segment.text[i].lstrip()) > 0 and trim_size > space_nr:
+                trim_size = space_nr
+            i += 1
+        return trim_size
+
+    def max_line_length(self, segment, trim_size, trim_start):
+        i = trim_start
+        while i < len(segment.text) - 1:
+            if len(segment.text[i]) > trim_size:
+                trim_size = len(segment.text[i])
+            i += 1
+        return trim_size
 
     def handle(self, pass_nr, segment):
         segment.text = self.postprocess(segment)
